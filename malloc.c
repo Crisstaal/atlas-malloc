2fef4b2739aea3a4874b4eceb36a673b6bfcb2c5
Crisstaal <144152402+Crisstaal@users.noreply.github.com>
2025-01-29 22:25:50 -0600

    try

--- malloc.c
+++ malloc.c
@@ -4,7 +4,11 @@
 
 #define PAGE_SIZE 4096
 
-/** Block header structure for managing memory blocks **/
+/**
+ * struct block - Block header structure for managing memory blocks
+ * @size: Size of the allocated memory block
+ * @next: Pointer to the next block in the free list
+ */
 typedef struct block {
     size_t size;
     struct block *next;
@@ -14,44 +18,46 @@ typedef struct block {
 static block_t *free_list = NULL;
 
 /**
- * _malloc - Allocates memory in page-sized chunks with a header
+ * _malloc - Allocates memory using sbrk
  * @size: The number of bytes
  *
  * Return: A pointer to the allocated memory or NULL if the allocation fails.
  */
 void *_malloc(size_t size)
 {
-    /** If size is 0, return NULL **/
-    if (size == 0) {
-        return NULL;
-    }
-
-    /** Calculate the total size to be allocated: header size + requested size **/
-    size_t total_size = size + sizeof(block_t);
-
-    /** Try to find a suitable free block in the free list **/
     block_t *prev = NULL, *curr = free_list;
-    while (curr && curr->size < total_size) {
+    block_t *new_block;
+    size_t total_size;
+
+    if (size == 0)
+        return (NULL);
+
+    /** Calculate total allocation size **/
+    total_size = size + sizeof(block_t);
+
+    /** Search for a suitable free block **/
+    while (curr && curr->size < total_size)
+    {
         prev = curr;
         curr = curr->next;
     }
 
-    if (curr) {
-        /** Found a suitable free block, remove it from the free list **/
-        if (prev) {
+    if (curr)
+    {
+        /** Remove the block from the free list **/
+        if (prev)
             prev->next = curr->next;
-        } else {
+        else
             free_list = curr->next;
 
-        return (void *)(curr + 1);
-        }
+        return ((void *)(curr + 1));
     }
-        /** No suitable free block, expand the heap **/
-    block_t *new_block = sbrk(total_size);
-    if (new_block == (void *)-1){
-        return NULL;
+
+    /** No suitable free block, expand the heap **/
+    new_block = sbrk(total_size);
+    if (new_block == (void *)-1)
+        return (NULL);
 
     new_block->size = total_size;
-    return (void *)(new_block + 1);
-}
+    return ((void *)(new_block + 1));
 }
