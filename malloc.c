6906f9f040dcd6a1e963fdb84dd6b32905abccaa
Crisstaal <muyungacrys@gmail.com>
2025-02-12 22:56:55 -0600

    save

--- malloc.c
+++ malloc.c
@@ -9,13 +9,27 @@
  * @size: Size of the allocated memory block
  * @next: Pointer to the next block in the free list
  */
-typedef struct block {
-    size_t size;
-    struct block *next;
+typedef struct block
+{
+	size_t size;
+	struct block *next;
 } block_t;
 
 /** Static free list to manage available memory **/
 static block_t *free_list = NULL;
+
+/**
+ * align_size - Aligns an unsigned value to the ceiling multiple of 8
+ * @size: The size to align
+ *
+ * Return: The aligned size
+ */
+size_t align_size(size_t size)
+{
+	size += 7;
+	size /= 8;
+	return size * 8;
+}
 
 /**
  * _malloc - Allocates memory using sbrk
@@ -25,39 +39,44 @@ static block_t *free_list = NULL;
  */
 void *_malloc(size_t size)
 {
-    block_t *prev = NULL, *curr = free_list;
-    block_t *new_block;
-    size_t total_size;
+	block_t *prev = NULL, *curr = free_list;
+	block_t *new_block;
+	size_t total_size;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    /** Calculate total allocation size **/
-    total_size = size + sizeof(block_t);
+	/** Align the requested size to the nearest multiple of 8 **/
+	size = align_size(size);
 
-    /** Search for a suitable free block **/
-    while (curr && curr->size < total_size)
-    {
-        prev = curr;
-        curr = curr->next;
-    }
+	/** Calculate total allocation size, including the block header **/
+	total_size = size + sizeof(block_t);
 
-    if (curr)
-    {
-        /** Remove the block from the free list **/
-        if (prev)
-            prev->next = curr->next;
-        else
-            free_list = curr->next;
+	/** Search for a suitable free block **/
+	while (curr && curr->size < total_size)
+	{
+		prev = curr;
+		curr = curr->next;
+	}
 
-        return ((void *)(curr + 1));
-    }
+	if (curr)
+	{
+		/** Remove the block from the free list **/
+		if (prev)
+			prev->next = curr->next;
+		else
+			free_list = curr->next;
 
-    /** No suitable free block, expand the heap **/
-    new_block = sbrk(total_size);
-    if (new_block == (void *)-1)
-        return (NULL);
+		return ((void *)(curr + 1));
+	}
 
-    new_block->size = total_size;
-    return ((void *)(new_block + 1));
+	/** No suitable free block, expand the heap **/
+	new_block = sbrk(total_size);
+	if (new_block == (void *)-1)
+		return (NULL);
+
+	new_block->size = total_size;
+
+	/** Always return the pointer to the allocated memory, not the block itself **/
+	return ((void *)(new_block + 1));
 }
